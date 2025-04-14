cca0a08ee3f3d6191e89b5779e21192f377d9ada
박상기 <sangki930@hanmail.net>
2025-04-14 23:39:58 +0900

    막대기

---
+++ src/math/막대기/Main.kt
@@ -0,0 +1,23 @@
+package math.막대기
+
+import java.util.Scanner
+
+fun main() {
+    val sc = Scanner(System.`in`)
+
+    var input = sc.nextInt()
+    var i = 6 + 1
+    var answer = 0
+
+    while (i-- > 0) {
+        // println("i의값 : $i")
+        if (input / (1 shl i) == 0) {
+            continue
+        } else {
+            input = input - (1 shl i)
+            answer++
+        }
+    }
+
+    println(answer)
+}
